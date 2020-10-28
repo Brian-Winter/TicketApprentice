9500312b227b2b1205362792b85a6558df3cdf66
Brian Winter <pawndesigns@gmail.com>
2020-10-28 19:48:58 -0400

    ticket entity added

---
+++ BlueBadge.Data/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadge.Data
+{
+    public enum TicketTier
+    {
+        General, 
+        LowerLevel,
+        VIP
+    }
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+        [ForeignKey(nameof(Event))]
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        public decimal Cost { get; set; }
+        public string SeatName { get; set; }
+    }
+}
