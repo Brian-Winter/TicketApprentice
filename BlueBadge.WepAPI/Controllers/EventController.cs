d1d9a8271d7788397f6a0859d480808a94f5753a
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-11-02 19:07:33 -0500

    Added Event models, service, and controller

---
+++ BlueBadge.WepAPI/Controllers/EventController.cs
@@ -0,0 +1,39 @@
+﻿using BlueBadge.Models.Event;
+using BlueBadge.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace BlueBadge.WepAPI.Controllers
+{
+    [Authorize]
+    public class EventController : ApiController
+    {
+        private EventService CreateEventService(int venueId)
+        {
+            var eventService = new EventService(venueId);
+            return eventService;
+        }
+
+        // Create
+        public IHttpActionResult Post(EventCreate newEvent, [FromUri] int venueId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var eventService = CreateEventService(venueId);
+
+            if (!eventService.CreateEvent(newEvent))
+                return InternalServerError();
+
+            return Ok(newEvent);
+        }
+
+        // Read
+        // Update 
+        // Delete
+    }
+}
