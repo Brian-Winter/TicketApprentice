3627bec6d80fec36b8a2fd9e1807a033630f064f
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-11-04 22:23:17 -0500

    Added Get Event by id and Get Event by Venue Working on adding Get Even by date

--- BlueBadge.WepAPI/Controllers/EventController.cs
+++ BlueBadge.WepAPI/Controllers/EventController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNet.Identity;
 namespace BlueBadge.WepAPI.Controllers
 {
     [Authorize]
+    [RoutePrefix("api/Event")]
     public class EventController : ApiController
     {
         private EventService CreateEventService()
@@ -38,6 +39,34 @@ namespace BlueBadge.WepAPI.Controllers
         {
             EventService eventService = CreateEventService();
             var events = eventService.GetAllEvents();
+            return Ok(events);
+        }
+
+        [Route("{type}/{id}")]
+        public IHttpActionResult Get(string type, int id)
+        {
+            var eventService = CreateEventService();
+
+            switch (type)
+            {
+                case "e":
+                    var e = eventService.GetEventById(id);
+                    return Ok(e);
+                    
+                case "v":
+                    var ev = eventService.GetEventsByVenue(id);
+                    return Ok(ev);
+
+                default:
+                    return BadRequest("The request is invalid, please specify your search parameters. (Try \"event\" or \"venue\")");
+            }
+        }
+
+        [Route("date")]
+        public IHttpActionResult Get([FromBody] DateTime date)
+        {
+            var eventService = CreateEventService();
+            var events = eventService.GetEventsByDate(date);
             return Ok(events);
         }
 
